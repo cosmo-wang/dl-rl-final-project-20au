5e66cf9ad4c8a7a1393918091df0d6eb04a09596
guohaz-1662769 <guohaz@uw.edu>
2020-12-02 22:58:15 -0800

    Checked ResNet

--- src/ResNet.py
+++ src/ResNet.py
@@ -76,7 +76,7 @@ class BasicBlock(nn.Module):
 
 
 class Bottleneck(nn.Module):
-    def __init__(self, type, in_channels, out_channels, stride=1, expansion=1):
+    def __init__(self, type, in_channels, out_channels, stride=1, expansion=4):
         super(Bottleneck, self).__init__()
         if type == "actor":
             conv = normed_conv
@@ -129,13 +129,12 @@ class ResNet(nn.Module):
             relu = TReLU()
 
         self.conv1 = conv(num_inputs, 64, 2)
+        self.relu1 = relu()
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=2)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.fc = nn.Linear(512, num_outputs)
-
-        self.relu1 = relu()
 
     def _make_layer(self, block, out_channels, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
